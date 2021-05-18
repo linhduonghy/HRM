b49766f857419e6034e7d026bcfd8b401000ea06
unknown <linhhahoang1999@gmail.com>
2021-05-19 01:05:07 +0700

    a

--- src/main/java/com/cuder/controller/EmployeeController.java
+++ src/main/java/com/cuder/controller/EmployeeController.java
@@ -2,6 +2,10 @@ package com.cuder.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
+import com.cuder.model.Allowance;
 import com.cuder.model.Appointment;
+import com.cuder.model.Member;
 import com.cuder.model.Staff;
 import com.cuder.model.Title;
 
@@ -28,29 +34,45 @@ public class EmployeeController {
 		// add model staff
 		Staff[] staffs = rest.getForObject("http://localhost:8081/staff", Staff[].class);
 		model.addAttribute("staffs", staffs);
-		
+
 		List<Title> titles = new ArrayList<Title>();
-		
-		for(int i =0;i < staffs.length;i++) {
-			
+
+		for (int i = 0; i < staffs.length; i++) {
+
 			List<Appointment> appoint = staffs[i].getAppointments();
-			titles.add(appoint.get(appoint.size()-1).getTitle());
+			titles.add(appoint.get(appoint.size() - 1).getTitle());
 		}
 		model.addAttribute("titles", titles);
-		
+
 		return "employee/employee.html";
 	}
-	
+
 	@GetMapping("/profile/{id}")
-	public String showFrofile(Model model,@PathVariable("id") int id	) {
+	public String showFrofile(Model model, @PathVariable("id") int id) {
 		// add model staff
-		Staff staff = rest.getForObject("http://localhost:8081/staff/{id}", Staff.class,id);
+		Staff staff = rest.getForObject("http://localhost:8081/staff/{id}", Staff.class, id);
 		List<Appointment> appoint = staff.getAppointments();
-		String title = appoint.get(appoint.size()-1).getTitle().getTitle_name();
-		model.addAttribute("staff",staff);
-		model.addAttribute("title",title);
-		
+		String title = appoint.get(appoint.size() - 1).getTitle().getTitle_name();
+		model.addAttribute("staff", staff);
+		model.addAttribute("title", title);
+
 		return "employee/edit.html";
+	}
+
+	@PostMapping("/edit")
+	public String editEmployee(@ModelAttribute Staff staff, @RequestParam Map<String, String> requestParams,
+			HttpSession session,HttpServletRequest request) {
+
+		System.out.println(staff);
+		Member member = staff.getMember();
+		System.out.println(member);
+
+        rest.put("http://localhost:8081/staff/{id}", Staff.class, staff.getId());
+
+		System.out.println(staff);
+		session.setAttribute("msg", "1");
+	    String referer = request.getHeader("Referer");
+	    return "redirect:"+ referer;
 	}
 
 }
