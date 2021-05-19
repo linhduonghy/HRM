822a34282ee96841e4dd5b051ec4a24f5cbbbd5a
unknown <linhhahoang1999@gmail.com>
2021-05-19 12:12:57 +0700

    a

--- src/main/java/com/cuder/controller/LoginController.java
+++ src/main/java/com/cuder/controller/LoginController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.cuder.model.Allowance;
 import com.cuder.model.Appointment;
+import com.cuder.model.Contract;
 import com.cuder.model.Member;
 import com.cuder.model.Staff;
 import com.cuder.model.Title;
@@ -30,21 +32,40 @@ public class LoginController {
 
 	// get list staff
 	@GetMapping("")
-	public String showloginForm(Model model) {
+	public String showloginForm(Model model, HttpSession session) {
 		// add model staff
 
 		model.addAttribute("member", new Member());
+		if (session.getAttribute("logMsg") != null) {
+			String s = (String) session.getAttribute("logMsg");
+			model.addAttribute("msg", s);
+			session.removeAttribute("logMsg");
+		}
 
 		return "user/login.html";
 	}
 
 	@PostMapping("/check")
-	public String showFrofile(Model model,@ModelAttribute("member") Member member) {
-		// 
-		
+	public String showFrofile(Model model, @ModelAttribute("member") Member member, HttpSession session) {
+		//
 
-		return "employee/edit.html";
+		Member logMember = rest.getForObject("http://localhost:8081/member/usrn/{usrn}", Member.class,
+				member.getUsername());
+
+		if (logMember == null) {
+			session.setAttribute("logMsg", "user");
+
+			return "redirect:/login";
+		}
+
+		if (!logMember.getPassword().equals(member.getPassword())) {
+			session.setAttribute("logMsg", "password");
+
+			return "redirect:/login";
+
+		}
+		session.setAttribute("user", logMember);
+		return "redirect:/";
 	}
 
-	
 }
